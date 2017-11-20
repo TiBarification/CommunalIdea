e51ba66a076250e30bea576185c1f8ffaf3a0eec
kyryloantoshyn <akirill98@gmail.com>
2017-11-20 23:48:50 +0200

    Working authentication is done.

--- projectBOSE/ViewModel/MainViewModel.cs
+++ projectBOSE/ViewModel/MainViewModel.cs
@@ -25,17 +25,11 @@ namespace projectBOSE.ViewModel
             //_viewModelPage = pagesList[0];
             _viewModelPage = controlsList[0];
             ViewModelLocator.Instance.AuthenticationViewModel.Identifying += AuthenticationViewModel_Identifying;
-            ViewModelLocator.Instance.Page3ButtonsViewModel.OnPaymentClicked += Page3ButtonsViewModel_OnPaymentClicked;
         }
 
         void AuthenticationViewModel_Identifying(object sender, EventArgs e)
         {
             ViewModelPage = controlsList[1];
-        }
-
-        void Page3ButtonsViewModel_OnPaymentClicked(object sender, EventArgs e)
-        {
-            ViewModelPage = controlsList[2];
         }
 
 
@@ -57,7 +51,7 @@ namespace projectBOSE.ViewModel
         //          }));
         //    }
         //}
-
+        
         //public event PropertyChangedEventHandler PropertyChanged;
         //public void OnPropertyChanged([CallerMemberName]string prop = "")
         //{
@@ -67,12 +61,7 @@ namespace projectBOSE.ViewModel
 
 
         //private List<Page> pagesList = new List<Page>() {new PageAuthentication(), new Page3Buttons() };
-        private List<UserControl> controlsList = new List<UserControl>() {
-            new Controls.UserControlAuthentication(),
-            new Controls.UserControl3Buttons(),
-            // add pay control
-            new Controls.UserControlPay(),
-        };
+        private List<UserControl> controlsList = new List<UserControl>() { new Controls.UserControlAuthentication(), new Controls.UserControl3Buttons() };
 
         /// <summary>
         /// The <see cref="ViewModelPage" /> property's name.
